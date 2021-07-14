a152e89088152d4a58f3440dfc4de089d3317cce
amorichard13 <richardamo13@gmail.com>
2021-07-14 12:02:53 -0700

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,16 @@
+#include <stdio.h>
 #include "holberton.h"
+
 /**
- *main - entry point.
- *Description: program starts and ends here.
- *Return: 0 on success.
+ * main - Entry point
+ *
+ * _putchar: Calls function _putchar in hoberton.h
+ *
+ * Return: it return a zero
  */
+
 int main(void)
 {
-char text[9] = "Holberton";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(text[i]);
-}
-_putchar('\n');
+_putchar('Holberton\n');
 return (0);
 }
